64d35c9d1f6763792d9b08349700e4d68ea5ad04
Ainesh Adhikari <20051482@kiit.ac.in>
2021-11-01 23:06:43 +0530

    Week3

---
+++ Assets/Ainesh_Adhikari/Week3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    public float speed;
+    public TextMeshProUGUI countText;
+    public GameObject winText;
+    public AudioClip collectcoin;
+    private int count;
+    public GameObject effect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winText.SetActive(false);
+        effect.gameObject.SetActive(false);
+
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+
+    }
+
+
+    void SetCountText()
+    {
+        countText.text = "Score : " + count.ToString();
+        if (count >= 12)
+        {
+            winText.SetActive(true);
+            effect.gameObject.SetActive(true);
+        }
+            
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp")){
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.clip = collectcoin;
+            if (other.gameObject.activeSelf)
+                audio.Play();
+
+            other.gameObject.SetActive(false);
+
+            count++;
+            SetCountText();
+        }
+        
+
+    }
+}
